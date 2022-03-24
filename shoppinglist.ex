3c2f1d756e603979ded2d6da362bff041d999ca5
cheropChirchir7963 <cheropmarlene@gmail.com>
2022-03-24 22:39:31 +0300

    added function to view items in cart

--- shoppinglist.ex
+++ shoppinglist.ex
@@ -9,6 +9,14 @@ defmodule ShoppingList do
         GenServer.cast(pid, item)
     end
 
+    def view(pid) do
+        GenServer.call(pid, :view)
+    end
+
+    def handle_call(:view, _from, list) do
+        {:reply, list, list }
+    end
+
     def handle_cast(item, list) do
         updated_list = [item|list]
         {:noreply, updated_list}
