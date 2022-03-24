54825b1630498676832407005bcef5a7f4be57d7
cheropChirchir7963 <cheropmarlene@gmail.com>
2022-03-24 20:23:24 +0300

    added function to add items to shopping list

--- shoppinglist.ex
+++ shoppinglist.ex
@@ -5,6 +5,15 @@ defmodule ShoppingList do
         GenServer.start_link(__MODULE__, [])
     end
 
+    def add(pid, item) do
+        GenServer.cast(pid, item)
+    end
+
+    def handle_cast(item, list) do
+        updated_list = [item|list]
+        {:noreply, updated_list}
+    end
+
     def init(list) do
         {:ok, list}
     end
